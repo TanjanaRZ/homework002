d94b583bfdaa508f03082d903ec1182f358c86cf
Tatjana <taninjawi@mail.ru>
2022-08-16 17:04:50 +0700

    Добавила задачу 23

---
+++ task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= N)
+{
+    Console.Write($"{Math.Pow(count,3)}    ");
+    count++;
+}
